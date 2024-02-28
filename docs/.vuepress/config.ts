d7b8365b87f2d317a14f823659e826765e307e1b
Thomas <chenzh0605@foxmail.com>
2024-02-28 23:16:43 +0800

    feat: 升级 vuepress 版本

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,11 +1,10 @@
-import { defineUserConfig } from 'vuepress'
-import type { DefaultThemeOptions } from 'vuepress'
-import { navbar, sidebar } from './configs'
-import  copyCode  from 'vuepress-plugin-copy-code2'
-const { docsearchPlugin } = require('@vuepress/plugin-docsearch')
-const { defaultTheme } = require('@vuepress/theme-default')
+import { navbar, sidebar } from './configs';
+import { docsearchPlugin } from '@vuepress/plugin-docsearch';
+import { defaultTheme } from '@vuepress/theme-default';
+import { viteBundler } from '@vuepress/bundler-vite';
+import { defineUserConfig } from 'vuepress';
 
-module.exports = {
+export default defineUserConfig({
   base: '/document/',
   public: 'docs/.vuepress/public',
 
@@ -14,13 +13,14 @@ module.exports = {
   title: 'Document',
   description: '',
 
+  bundler: viteBundler(),
   // 主题和它的配置
   theme: defaultTheme({
     logo: 'images/logo/logo.png',
     // 导航栏配置
     navbar: navbar.zh,
     // 侧边栏配置
-    sidebar: sidebar.zh,
+    sidebar: 'auto', // sidebar.zh
 
     // 参与编辑页信息相关
     // 项目仓库的 URL
@@ -35,7 +35,7 @@ module.exports = {
     editLinkText: '在 GitHub 上编辑此页',
     lastUpdated: true,
     lastUpdatedText: '上次更新',
-    contributors: true,
+    contributors: false,
     contributorsText: '贡献者',
 
     // 自定义容器缺省文本
@@ -44,21 +44,15 @@ module.exports = {
     danger: '警告',
 
     // 404 页面随机显示文本
-    notFound: [
-      '兄弟不要乱搞哟🔞🍌',
-      '进错地方了⛔',
-    ],
+    notFound: ['兄弟不要乱搞哟🔞🍌', '进错地方了⛔'],
     backToHome: '返回首页',
   }),
   plugins: [
-    copyCode({
-      duration: 2500,
-      pure: true,
-    }),
+    // Algolia DocSearch 网站搜索功能
     docsearchPlugin({
       apiKey: 'b33f6df6a3320c1166275edae4db2d80',
-      appId:'9XT7J1NNVO',
-      indexName:'fschenzihao',
+      appId: '9XT7J1NNVO',
+      indexName: 'fschenzihao',
       placeholder: '搜索文档',
       translations: {
         button: {
@@ -66,5 +60,5 @@ module.exports = {
         },
       },
     }),
-  ]
-}
+  ],
+});
